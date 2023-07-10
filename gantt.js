aad599a7d0a7e6d501f1383218a7e0c061ac2475
Sunil Solanki <sunil.solanki@zehntech.com>
2023-07-10 12:35:15 +0530

    fixed the task width issue

--- gantt.js
+++ gantt.js
@@ -5300,7 +5300,7 @@
     getPxByTime: function (date, type) {
       let hours = new Date(date).getHours();
       if(type === "width"){
-        hours = 24 - hours;
+        hours = hours === 0 ? 0 : 24 - hours;
       }
       let pxPerHour = this.calculateGridWidth(date, "day") / 24;
       let pixels = hours * pxPerHour;
