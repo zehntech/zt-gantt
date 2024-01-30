bf04ddc1e0feb6d2715e92025f7ee581ac1b6d5f
Sunil Solanki <sunil.solanki@zehntech.com>
2024-01-30 18:31:27 +0530

    update: row height dynamic

--- gantt.js
+++ gantt.js
@@ -9866,7 +9866,7 @@
 
       let that = this;
       this.options.data.forEach((task) => {
-        totalGanttHeight += 24;
+        totalGanttHeight += this.options.row_height;
         if (this.options.openedTasks.includes(task.id)) {
           totalGanttHeight += calculateVisibleTasksHeight(task);
         }
@@ -9875,7 +9875,7 @@
       function calculateVisibleTasksHeight(task) {
         let childHight = 0;
         if (that.options.openedTasks.includes(task.id)) {
-          childHight += task.children.length * 24;
+          childHight += task.children.length * that.options.row_height;
           task.children.forEach((child) => {
             childHight += calculateVisibleTasksHeight(child);
           });
